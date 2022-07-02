c9d8b4dfabfc70b3222fe8b53969317c9351a8b2
Beixi <beixizhang@gmail.com>
2022-07-02 16:45:25 -0700

    added a caption list and buttons

--- ez_captions/src/index.js
+++ ez_captions/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( // calls and loads the app component 
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
