35eaaebcf140c536b4502858e683fa96bee0f5f3
Joshua Revilla <were1708@yahoo.com>
2022-07-04 20:01:55 -0700

    updated UI double click to open menu

--- ez_captions/src/App.js
+++ ez_captions/src/App.js
@@ -13,24 +13,27 @@ const App = () => {
         id: 1,
         start: "00:00",
         end: "00:03",
-        text: "[Off-screen voice] Hello there!"
+        text: "[Off-screen voice] Hello there!",
+        edit: false
     },
     {
       id: 2,
         start: "00:07",
         end: "00:12",
-        text: "I am talking to you right now,"
+        text: "I am talking to you right now,",
+        edit: false
     },
     {
         id: 3,
         start: "00:12",
         end: "00:16",
-        text: "on the internet!!"
+        text: "on the internet!!",
+        edit: false
     }
     ])
     
     const addCaption = (caption) => { // add a new caption and give it a random ID
-        const id = Math.floor(Math.random() * 10000) + 1
+        const id = Math.floor(Math.random() * 10000) + 1 // we need to give the new caption a random ID
         const newCaption = {id, ...caption}
         setCaptions([...captions, newCaption]) // update the captions list as the previous list AND the new caption
         //console.log(caption) //THIS WAS FOR DEBUGGING
@@ -53,13 +56,19 @@ const App = () => {
       id)) // filters by keeping all captions that aren't the deleted captions ID
     }
 
+    const editCaption = (id) => { // enables editing of captions
+      setCaptions(captions.map((caption) => caption.id ===
+      id ? {...caption, edit: !caption.edit} : caption))
+      console.log(id)
+    }
+
     return (
 
       <div className='container'>
         <Header onClick={() => downloadCaptions(captions)}>Download Captions</Header>
         <NewCaption onAdd={addCaption}/> {/* submission form with onAdd prop for the submit button */}
         {captions.length > 0 ? // quick if statement for when there are no captions in the tool!
-        <Captions captions={captions} onDelete={deleteCaption} /> :
+        <Captions captions={captions} onDelete={deleteCaption} onToggle ={editCaption} onAdd={editCaption}/> :
         'Please input caption info!'}
         <SubmitFile />
       </div>
--- ez_captions/src/containers/Captions.js
+++ ez_captions/src/containers/Captions.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Caption from './Caption.js'
 
-const Captions = ({ captions, onDelete}) => {
+const Captions = ({ captions, onDelete, onToggle, onAdd}) => {
   // This is how the list of captions is rendered. A mapped list of caption objects
   // we also pass in a onDelete prop for when the Applications asks to delete a certain
   // caption with a certain ID
   return (
     <>
     {captions.map((caption) => (
-    <Caption caption={caption} onDelete={onDelete} />))}
+    <Caption caption={caption} onDelete={onDelete} onToggle={onToggle} onAdd={onAdd}/>))}
     </>
   )
 }
